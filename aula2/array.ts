2574fe186f183750e36b6efd94ace270d3222f66
Alef Adonais <alefadonais5@gmail.com>
2024-06-28 14:22:13 -0300

    Atualização

--- aula2/array.ts
+++ aula2/array.ts
@@ -1,5 +1,35 @@
-const array: string[] = [];
+// Questão 1
+let numeros: number[] = [];
 
-array.push("Alef");
+// Questão 2
+numeros = [1, 2, 3, 4];
 
-console.log(array);
+// Questão 3 
+console.log(numeros[1]);
+
+// Questão 4
+numeros[2] = 10;
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+5. Remova o último elemento do array "numeros" utilizando a função ".pop".
+6. Verifique o comprimento do array "numeros".
+7. Crie um novo array chamado "frutas" contendo as strings "maçã", "banana" e "laranja".
+8. Acesse o valor do primeiro elemento do array frutas.
+9. Adicione a string "uva" ao final do array frutas utilizando a função ".push".
+10. Crie um objeto chamado "aluno" com as propriedades "nome" e "idade" e atribua valores a elas.
+Em seguida, crie um array chamado "alunos" e adicione o objeto "aluno" a esse array.
+11. Acesse o valor da propriedade "nome" do primeiro elemento do array "alunos".
+12. Adicione mais três objetos do tipo "aluno" ao array "alunos".
+13. Crie um array chamando "produtos" contendo 10 produtos de supermercado. Em seguida, exiba no
+terminal apenas os produtos que ficaram em posições pares. */
