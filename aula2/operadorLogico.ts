4123978d680d0f00a6002d3f8b335d7fc5a9d3ea
alefadonais5 <alefadonais5@gmail.com>
2024-06-30 16:58:21 -0300

    Atualização

---
+++ aula2/operadorLogico.ts
@@ -0,0 +1,46 @@
+//Questão 1
+let numero: number = 4;
+let numero2: number = 10;
+console.log(numero==numero2);
+
+//Questão 2
+console.log(numero != numero2);
+
+//Questão 3
+console.log(numero > numero2);
+
+//Questão 4
+console.log(numero < numero2);
+
+//Questão 5
+console.log(numero >= numero2);
+
+//Questão 6
+console.log(numero <= numero2);
+
+//Questão 7
+console.log(numero > 0 && numero2 > 0);
+
+//Questão 8
+console.log(numero > 0 || numero2 > 0);
+
+//Questão 9
+console.log(!(numero > 0 || numero2 > 0));
+
+//Questão 10
+console.log(5 >= numero && 10 <= numero2);
+
+// Questão 11
+console.log(numero < 5 || numero2 > 15);
+
+// Questão 12
+console.log(numero > 0);
+
+// Questão 13
+let stringVazia: string = "";
+console.log(stringVazia == "");
+console.log(stringVazia.length == 0);
+
+// Questão 14
+let variavelBooleana: boolean = true;
+console.log(typeof variavelBooleana == "boolean");
