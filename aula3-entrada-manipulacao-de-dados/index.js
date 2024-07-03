2a2930c6e3d0067b952aff89be1e314aab4f1224
Alef Adonais <alefadonais5@gmail.com>
2024-07-03 11:21:49 -0300

    Aula 03 - Entrada e Manipulação de Dados

---
+++ aula3-entrada-manipulacao-de-dados/index.js
@@ -0,0 +1,23 @@
+// const numero1 = Number(prompt("Digite o primeiro número: "));
+// const numero2 = Number(prompt("Digite o segundo número: "));
+// console.log(numero1 + numero2);
+// const numero1 = prompt("Digite o primeiro número: ");
+// console.log(typeof numero1);
+// const numero1Convertido = Number(numero1);
+// console.log(typeof numero1Convertido);
+// const numero2 = prompt("Digite o segundo número: ");
+// console.log(typeof numero2);
+// const numero2Convertido = Number(numero2);
+// console.log(typeof numero2Convertido);
+// console.log(numero1Convertido + numero2Convertido);
+// const numero2 = prompt("Digite o segundo número: ");
+// console.log(numero1 + numero2);
+// const frase = prompt("Digite uma frese: ");
+// console.log(frase);
+// console.log(typeof frase);
+// console.log(frase);
+var peso = Number(prompt("Digite o peso: "));
+var altura = Number(prompt("Digite a altura: "));
+var imc = peso / (altura * altura); //const imc = peso / (altura ** 2);
+alert("O imc é " + imc.toFixed(2));
+//alert('O imc é ${imc.}')
