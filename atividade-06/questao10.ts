0bd176f03b920125a22a8ae77b71099d9a2b33fe
Alef Adonais <alefadonais5@gmail.com>
2024-08-06 14:42:30 -0300

    exercicios

---
+++ atividade-06/questao10.ts
@@ -0,0 +1,11 @@
+// 10. Crie um programa que exiba a tabuada do 9 utilizando o while.
+
+const n1:number = 7; 
+let cont:number = 1;
+
+console.log(`TABUADA DO 7`)
+
+while (cont <= 10) {
+    console.log(`${n1} X ${cont} = ${n1 * cont}`);
+    cont++
+}
