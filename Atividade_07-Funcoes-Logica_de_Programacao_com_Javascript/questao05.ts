4a5457fa9c9b0247e4c7fdcb6d8a14d25391cfad
alefadonais5 <alefadonais5@gmail.com>
2024-08-20 21:52:26 -0300

    Atividade 07 - Funções

---
+++ Atividade_07-Funcoes-Logica_de_Programacao_com_Javascript/questao05.ts
@@ -0,0 +1,7 @@
+//5. Escreva uma função chamada "soma" que receba dois parâmetros (a e b) e retorne a soma deles.
+
+function soma(a:number, b:number) {
+    return a + b;
+}
+
+console.log(soma(5,6));
