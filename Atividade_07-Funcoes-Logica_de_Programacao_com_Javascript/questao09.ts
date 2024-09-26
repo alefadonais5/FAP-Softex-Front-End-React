89f5e0e08c0ca31a3504d231f668e97263d07934
Alef Adonais <alefadonais5@gmail.com>
2024-09-26 12:27:15 -0300

    feat: atualização na questão 10

--- Atividade_07-Funcoes-Logica_de_Programacao_com_Javascript/questao09.ts
+++ Atividade_07-Funcoes-Logica_de_Programacao_com_Javascript/questao09.ts
@@ -1,4 +1,4 @@
-// Elabore uma função chamada "calcularDesconto" que receba o valor de um produto e o
+// 9- Elabore uma função chamada "calcularDesconto" que receba o valor de um produto e o
 // percentual de desconto como parâmetros e retorne o valor com o desconto aplicado
 
 function calcularDesconto(value:number,percentage: number ) {
