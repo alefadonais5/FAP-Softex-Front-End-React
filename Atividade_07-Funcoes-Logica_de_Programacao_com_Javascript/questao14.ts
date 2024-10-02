c8bdbfc05f35181a0c363417e5c76448a4d24fa1
alefadonais5 <alefadonais5@gmail.com>
2024-10-01 21:56:24 -0300

    feat: Resolução da questão 14.

---
+++ Atividade_07-Funcoes-Logica_de_Programacao_com_Javascript/questao14.ts
@@ -0,0 +1,8 @@
+// 14. Crie uma função chamada "calcularIdade" que receba o ano de "nascimento" de uma pessoa como
+// parâmetro e retorne a idade atual.
+
+function calcularIdade(year: number){
+    return 2024 - year;
+}
+
+console.log(`Sua idade é ${calcularIdade(1994)}`);
