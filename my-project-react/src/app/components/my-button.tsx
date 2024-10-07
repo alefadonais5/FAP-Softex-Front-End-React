8786107f267cfa7638bbdc97b5cbdaa09692b06a
Alef Adonais <alefadonais5@gmail.com>
2024-10-07 11:21:37 -0300

    Componentes

---
+++ my-project-react/src/app/components/my-button.tsx
@@ -0,0 +1,6 @@
+
+export function MyButtom() {
+    return (
+      <button className = "border border-black px-6 py-3 bg-blue-400">Button</button>
+    );
+  }
---
+++ my-project-react/src/app/page.tsx
@@ -0,0 +1,13 @@
+import { MyButtom } from "./components/my-button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Página Home</h1>
+      <MyButtom />
+      <MyButtom />
+      <MyButtom />
+
+    </div>
+  );
+}
